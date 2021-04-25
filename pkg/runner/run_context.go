379a106aacc9157a1ec0737fb3c1d42b1705618e
Anders Renström <anders.renstroom@gmail.com>
2021-04-25 13:53:17 +0200

    Changed double quotes to ${{}} and added tests to make sure that it works properly

--- pkg/runner/run_context.go
+++ pkg/runner/run_context.go
@@ -223,14 +223,14 @@ func (rc *RunContext) newStepExecutor(step *model.Step) common.Executor {
 		}
 		rc.ExprEval = exprEval
 
-		runStep, err := rc.EvalBool(sc.Step.If)
+		runStep, err := rc.EvalBool(sc.Step.If.Value)
 		if err != nil {
 			common.Logger(ctx).Errorf("  \u274C  Error in if: expression - %s", sc.Step)
 			rc.StepResults[rc.CurrentStep].Success = false
 			return err
 		}
 		if !runStep {
-			log.Debugf("Skipping step '%s' due to '%s'", sc.Step.String(), sc.Step.If)
+			log.Debugf("Skipping step '%s' due to '%s'", sc.Step.String(), sc.Step.If.Value)
 			return nil
 		}
 
@@ -279,13 +279,13 @@ func (rc *RunContext) platformImage() string {
 func (rc *RunContext) isEnabled(ctx context.Context) bool {
 	job := rc.Run.Job()
 	l := common.Logger(ctx)
-	runJob, err := rc.EvalBool(job.If)
+	runJob, err := rc.EvalBool(job.If.Value)
 	if err != nil {
 		common.Logger(ctx).Errorf("  \u274C  Error in if: expression - %s", job.Name)
 		return false
 	}
 	if !runJob {
-		l.Debugf("Skipping job '%s' due to '%s'", job.Name, job.If)
+		l.Debugf("Skipping job '%s' due to '%s'", job.Name, job.If.Value)
 		return false
 	}
 
