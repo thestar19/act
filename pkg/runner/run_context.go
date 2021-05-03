9b063aa653b37f75c8d47c68a620997744468c54
Anders Renström <anders.renstroom@gmail.com>
2021-05-03 21:38:16 +0200

    testing

--- pkg/runner/run_context.go
+++ pkg/runner/run_context.go
@@ -226,7 +226,7 @@ func (rc *RunContext) newStepExecutor(step *model.Step) common.Executor {
 			return err
 		}
 		if !runStep {
-			log.Debugf("Skipping step '%s' due to '%s'", sc.Step.String(), sc.Step.If.Value)
+			log.Debugf("Skipping step '%s' due to '%s'", sc.Step.Name, sc.Step.If.Value)
 			return nil
 		}
 
